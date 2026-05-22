05965204077c7a51630bfacc6441dc2a268bc3f4
Jasumy <jazmin.segundo.e@gmail.com>
2026-05-21 21:28:16 -0600

    Correción

--- platillos/models.py
+++ platillos/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Platillo(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=8, decimal_places=2)
+
+    categoria = models.ForeignKey(
+        Categoria,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    disponible = models.BooleanField(default=True)
+
+    imagen = models.ImageField(
+        upload_to='platillos/',
+        null=True,
+        blank=True
+    )
+
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre
